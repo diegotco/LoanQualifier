4e20220ae039b203fe4c5731bf9a6ceb3cd58da8
diegotco <diegotco@yahoo.com>
2021-02-05 13:38:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+/*
+
+1. Person needs to earn at least 30k dollars a year
+2. Person works for the same company at least for 2 years
+
+*/
+import java.util.Sacanner;
+
+class Main {
+  public static void main(String[] args) {
+    //Initializa what we know
+    int requiredSalary = 30000;
+    int requireWorkYears = 2;
+
+
+    //Get what we don't know
+    System.out.println("Enter your salary: ");
+    Scanner scanner = new Scanner(System.in);
+    double salary = scanner.nextdouble();
+
+    System.out.println("Enter the number of years you are working with you current employer: ");
+
+
+    //Make decisions - Get the outcome
+    
+
+  }
+}
